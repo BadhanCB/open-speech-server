07d093c450a569e4b92841e13109c5aa4a13d0ff
BadhanCB <badhancsesastc112@gmail.com>
2023-01-18 19:35:56 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+const mongoURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tinfh.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.set("strictQuery", false);
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch(e => console.error(e));
 
 const title = "<h1>This is Node js app</h1>";
 
@@ -10,4 +23,4 @@ app.use("/bcb", (req, res) => {
 
 app.listen(5000, () => {
     console.log("Server Running");
-})
+});

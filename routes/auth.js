1769c571feceede0c1e5c785896a679340eebadc
BadhanCB <badhancsesastc112@gmail.com>
2023-01-19 11:59:47 +0600

    add auth/login route api

--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,8 @@ const bcrypt = require("bcrypt");
 
 //register
 router.post("/register", async (req, res) => {
-    try{
-        const  {username, email, password, photoURL } = req?.body;
+    try {
+        const { username, email, password, photoURL } = req?.body;
         const salt = await bcrypt.genSalt(10);
         const hashPass = await bcrypt.hash(password, salt);
 
@@ -18,9 +18,30 @@ router.post("/register", async (req, res) => {
 
         const user = await newUser.save();
         res.status(200).json(user);
-
     } catch (err) {
         res.status(500).json(err);
+    }
+});
+
+//login
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req?.body?.username });
+        if(!user) {
+            res.status(400).json("wrong credenrials");
+            return;
+        }
+        
+        const validated = await bcrypt.compare(req?.body?.password, user.password);
+        if(!validated) {
+            res.status(400).json("wrong credenrials");
+            return;
+        }
+        
+        const {password, ...rest} = user._doc;
+        res.status(200).json(rest);
+    } catch (e) {
+        res.status(500).json(e);
     }
 });
 
